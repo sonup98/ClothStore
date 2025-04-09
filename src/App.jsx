d93a0eeb0e5ad8e42e931bedd829c35aad23fbe1
sonu <sonuprajapat2398@gmail.com>
2025-04-09 14:39:51 +0530

    Improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Hero from "./components/Hero"
+import NavBar from "./components/NavBar"
+import SideBarMenu from "./components/SideBarMenu"
 
 const App = () => {
   return (
-   <main className='relative min-h-screen w-screen overflow-x-hidden'>
+<div class=" bg-[#f3f3f1]">
+ 
+  <main class="col-span-5 p-6 mx-10 ">
+    <NavBar />
+    <SideBarMenu />
+    
     <Hero />
-
-   </main>
+  </main>
+</div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div class="flex items-center justify-between mb-10">
+
+  <div class="flex items-center gap-4">
+    <button class="text-xl">☰</button>
+    <nav class="flex gap-6 text-sm uppercase tracking-wide text-gray-700">
+      <a href="#">Home</a>
+      <a href="#">Collections</a>
+      <a href="#">New</a>
+    </nav>
+  </div>
+
+ 
+  <div>
+    <div class="rotate-45 w-4 h-4 bg-black"></div>
+  </div>
+
+  
+  <div class="flex items-center gap-4">
+    <button class="w-8 h-8 rounded-full border flex items-center justify-center">♡</button>
+    <div class="flex items-center gap-1 bg-black text-black px-2 py-1 rounded-full text-sm">
+      Cart
+      <span class="inline-block w-4 h-4 rounded-full bg-black text-black text-xs text-center">0</span>
+    </div>
+    <button class="w-8 h-8 rounded-full border flex items-center justify-center">👤</button>
+  </div>
+</div>
+
+  )
+}
+
+export default NavBar
